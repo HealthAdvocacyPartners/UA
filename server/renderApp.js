afbf27d61e508d7d84367dad52fac4297a7407f5
Will Chertoff <willchertoff@gmail.com>
2018-02-16 15:40:43 -0800

    syntax sugar

--- server/renderApp.js
+++ server/renderApp.js
@@ -11,56 +11,53 @@ import { Client, Provider } from './apollo'
 import Main from '../client/containers/Main'
 import Html from '../client/containers/Html'
 
-function RenderApp({ clientStats, serverStats, foo }) {
-  return (req, res, next) => {
-    const context = {}
-    const apolloClient = Client(req)
+// Entry point for server 👩🏻‍🍳
+const RenderApp = ({ clientStats, serverStats, foo }) => (req, res, next) => {
+  const apolloClient = Client(req)
 
-    const component = (
-      <Provider client={apolloClient}>
-        <StaticRouter location={req.url} context={context}>
-          <Main />
-        </StaticRouter>
-      </Provider>
-    )
+  const component = (
+    <Provider client={apolloClient}>
+      <StaticRouter location={req.url} context={{}}>
+        <Main />
+      </StaticRouter>
+    </Provider>
+  )
 
-    getDataFromTree(component)
-      .then(() => {
-        const app = ReactDOM.renderToString(component)
+  getDataFromTree(component)
+    .then(() => {
+      const app = ReactDOM.renderToString(component)
 
-        const chunkNames = flushChunkNames()
-        const { Js, Styles, scripts, stylesheets } = flushChunks(clientStats, {
-          chunkNames
-        })
-
-        console.log('PATH', req.path)
-        console.log('DYNAMIC CHUNK NAMES RENDERED', chunkNames)
-        console.log('SCRIPTS SERVED', scripts)
-        console.log('STYLESHEETS SERVED', stylesheets)
-
-        const helmet = Helmet.renderStatic()
-        const initialState = apolloClient.extract()
-
-        const html = (
-          <Html
-            app={app}
-            state={initialState}
-            helmet={helmet}
-            Js={Js}
-            Styles={Styles}
-          />
-        )
-
-        res.status(200)
-        res.send(`<!doctype html>\n${ReactDOM.renderToString(html)}`)
-        res.end()
+      const chunkNames = flushChunkNames()
+      const { Js, Styles, scripts, stylesheets } = flushChunks(clientStats, {
+        chunkNames
       })
-      .catch(e => {
-        console.log('Network error' + e)
-        res.redirect('/error')
-        res.end()
-      })
-  }
+      const helmet = Helmet.renderStatic()
+      const initialState = apolloClient.extract()
+
+      console.log('PATH', req.path)
+      console.log('DYNAMIC CHUNK NAMES RENDERED', chunkNames)
+      console.log('SCRIPTS SERVED', scripts)
+      console.log('STYLESHEETS SERVED', stylesheets)
+
+      const html = (
+        <Html
+          app={app}
+          state={initialState}
+          helmet={helmet}
+          Js={Js}
+          Styles={Styles}
+        />
+      )
+
+      res.status(200)
+      res.send(`<!doctype html>\n${ReactDOM.renderToString(html)}`)
+      res.end()
+    })
+    .catch(e => {
+      console.log('Network error' + e)
+      res.redirect('/error')
+      res.end()
+    })
 }
 
 export default RenderApp
